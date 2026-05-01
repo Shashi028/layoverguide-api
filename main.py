f9492b4fc0ea77cb2ac39f9265050a68ea38673a
Shashi Singh <singh.shashi2125@gmail.com>
2026-05-01 13:42:00 +0930

    add vercel url to CORS allowed origins

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="LayoverGuide API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://layoverguide-frontend.vercel.app/"],
+    allow_origins=["http://localhost:3000","https://layoverguide-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
